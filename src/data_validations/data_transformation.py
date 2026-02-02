7b98e38ec771d1e3cebff723952649fa557a44e9
shree <nimbaneshree@gmail.com>
2026-02-02 13:47:57 +0530

    first added and remove files

--- src/data_validations/data_transformation.py
+++
@@ -1,7 +0,0 @@
-
-
-
-class Data_Transformation:
-    'update time ? reference column check / different 1:1 transfer'
-    ''' Types :-  concatenations / joins / conditional Male in one table and female ot other/
-    aggregations / data convertion male to M and F/ filter salary >1000'''
